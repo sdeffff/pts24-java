a46f0ab5a750300ee05188113017abd91fd4b34d
Arsenii-Nemchenko <arsenii.nemchenko@gmail.com>
2024-12-01 13:08:59 +0200

    Added VariableBuilding unit test

--- src/test/java/sk/uniba/fmph/dcs/game_board/VariableBuildingTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/VariableBuildingTest.java
@@ -3,6 +3,8 @@ package sk.uniba.fmph.dcs.game_board;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
 import org.junit.Test;
 import sk.uniba.fmph.dcs.stone_age.Effect;
@@ -11,18 +13,44 @@ public class VariableBuildingTest {
 
     @Test
     public void test_calculation_1(){
-        ArrayList<Effect> buildingResources = new ArrayList<Effect>();
-        buildingResources.add(Effect.WOOD);
-        buildingResources.add(Effect.WOOD);
-        buildingResources.add(Effect.GOLD);
+        List<Effect> buildingResources = Arrays.asList(Effect.WOOD, Effect.WOOD, Effect.WOOD, Effect.GOLD);
 
+        VariableBuilding building = new VariableBuilding(4, 2);
 
-        VariableBuilding building = new VariableBuilding(3, 2);
+        assertEquals(building.build(buildingResources), OptionalInt.of(15));
 
-        assertEquals(building.build(buildingResources), OptionalInt.of(12));
+        assertEquals(building.state(), String.format("VariableBuilding[resources=%d,types=%d]", 4, 2));
+    }
 
-        ArrayList<Effect> otherResources = new ArrayList<Effect>();
-        assertEquals(building.build(otherResources), OptionalInt.empty());
+    @Test
+    public void test_calculation_2(){
+        List<Effect> buildingResources = Arrays.asList(Effect.WOOD, Effect.WOOD, Effect.GOLD);
+
+        VariableBuilding building = new VariableBuilding(4, 2);
+
+        assertEquals(building.build(buildingResources), OptionalInt.empty());
+
+        assertEquals(building.state(), String.format("VariableBuilding[resources=%d,types=%d]", 4, 2));
+    }
+    @Test
+    public void test_calculation_3(){
+        List<Effect> buildingResources = Arrays.asList(Effect.WOOD, Effect.CLAY, Effect.STONE, Effect.GOLD);
+
+        VariableBuilding building = new VariableBuilding(4, 4);
+
+        assertEquals(building.build(buildingResources), OptionalInt.of(18));
+
+        assertEquals(building.state(), String.format("VariableBuilding[resources=%d,types=%d]", 4, 4));
+    }
+    @Test
+    public void test_calculation_4(){
+        List<Effect> buildingResources = Arrays.asList(Effect.WOOD, Effect.CLAY, Effect.STONE);
+
+        VariableBuilding building = new VariableBuilding(4, 4);
+
+        assertEquals(building.build(buildingResources), OptionalInt.empty());
+
+        assertEquals(building.state(), String.format("VariableBuilding[resources=%d,types=%d]", 4, 4));
     }
 
 }
