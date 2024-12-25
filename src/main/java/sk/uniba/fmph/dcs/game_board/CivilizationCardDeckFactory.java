803d9791c30537bdafe9964ec45fddad56cb0dba
Vladyslav Havriuk <web.user@ukr.net>
2024-12-25 05:25:12 +0100

    Remove extra code block

--- src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeckFactory.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/CivilizationCardDeckFactory.java
@@ -34,23 +34,3 @@ public class CivilizationCardDeckFactory {
         return new CivilizationCardDeck(new Stack<>());
     }
 }
-    public static CivilizationCardDeck createProductionDeck(List<CivilizationCard> cards) {
-        Stack<CivilizationCard> stack = new Stack<>();
-        List<CivilizationCard> shuffledCards = new ArrayList<>(cards);
-        Collections.shuffle(shuffledCards);
-        stack.addAll(shuffledCards);
-        return new CivilizationCardDeck(stack);
-    }
-    
-    public static CivilizationCardDeck createTestDeck(List<CivilizationCard> orderedCards) {
-        Stack<CivilizationCard> stack = new Stack<>();
-        // Add in reverse order so they come out in the specified order
-        for (int i = orderedCards.size() - 1; i >= 0; i--) {
-            stack.push(orderedCards.get(i));
-        }
-        return new CivilizationCardDeck(stack);
-    }
-    
-    public static CivilizationCardDeck createEmptyDeck() {
-        return new CivilizationCardDeck(new Stack<>());
-    }
